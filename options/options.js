0632582bd4777b357bb2ef33dbba0b37d68177fe
Crowdbotics <team@crowdbotics.com>
2021-09-04 02:47:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "still_bread_30307Identifier",
+  url: "https://still_bread_30307Identifier.botics.co",
+  api: "https://still_bread_30307Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
